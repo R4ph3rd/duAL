c518cceb42c3f06fa0f46484dbda1f1a2386a65e
Florian DUFRESNE <dufresne.florian@gmail.com>
2021-12-01 19:07:25 +0100

    Added the dispenser class

---
+++ Assets/Scripts/Dispenser.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dispenser : MonoBehaviour
+{
+    public float ejectionForce = 10f;
+    public Vector3 ejectionDir = new Vector3(0, 0, 0);
+
+    public void LaunchProjectile()
+    {
+        GameObject projectile = DispenserManager.instance.InstantiateProjectile();
+        Projectile proj;
+        if(projectile.TryGetComponent<Projectile>(out proj))
+        {
+            proj.InitializeProjectile(ejectionDir, ejectionForce,this.transform.position);
+        }
+    }
+
+    
+}
