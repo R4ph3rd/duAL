ae766da15be066d3bb7f4b39f431daec5c272861
r4ph3rd <r.perraud@lecolededesign.com>
2021-12-02 11:04:26 +0100

    add teleport prefab zones

---
+++ Assets/Scripts/Managers/HumanManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public bool isTPavalaible = true;
+    public float TPDelay = 5.120f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void TeleportPower()
+    {
+        isTPavalaible = false;
+        StartCoroutine(resetTPpower());
+    }
+
+    IEnumerator resetTPpower()
+    {
+        yield return new WaitForSecondsRealtime(TPDelay);
+        isTPavalaible = true;
+    }
+}
