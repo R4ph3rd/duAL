cbac55107abc80b4bc2d4acd0cf6dac058dcacea
nadinesalame1 <nadine_salame@hotmail.com>
2021-12-03 01:35:04 +0100

    integration + teleport fix + helmet

--- Assets/Scripts/Managers/TPZoneManager.cs
+++ Assets/Scripts/Managers/TPZoneManager.cs
@@ -28,83 +28,50 @@ public class TPZoneManager : MonoBehaviour
     /// </summary>
     public void UpdateLockStates()
     {
-        switch (humanManager.roomID)
+        if (humanManager.isTPavalaible)
         {
-            case Room.bridge:
-                if (humanManager.isTPavalaible)
-                {
-                    foreach (TeleportPoint tp in bridgeTeleportPoints)
-                    {
-                        tp.SetLocked(false);
-                    }
-                }
-                else
-                {
-                    foreach (TeleportPoint tp in bridgeTeleportPoints)
-                    {
-                        tp.SetLocked(true);
-                    }
-                }
+            foreach (TeleportPoint tp in bridgeTeleportPoints)
+            {
+                tp.SetLocked(humanManager.roomID == Room.bridge);
+                SetTPPoint(tp, humanManager.roomID == Room.bridge);
+            }
 
-                foreach(TeleportPoint tp in controlRoomTeleportPoints)
-                {
-                    tp.SetLocked(true);
-                }
-                foreach (TeleportPoint tp in storageRoomTeleportPoints)
-                {
-                    tp.SetLocked(true);
-                }
-                break;
-            case Room.control:
-                if (humanManager.isTPavalaible)
-                {
-                    foreach (TeleportPoint tp in controlRoomTeleportPoints)
-                    {
-                        tp.SetLocked(false);
-                    }
-                }
-                else
-                {
-                    foreach (TeleportPoint tp in controlRoomTeleportPoints)
-                    {
-                        tp.SetLocked(true);
-                    }
-                }
+            foreach (TeleportPoint tp in controlRoomTeleportPoints)
+            {
+                tp.SetLocked(humanManager.roomID == Room.control);
+                SetTPPoint(tp, humanManager.roomID == Room.control);
+            }
 
-                foreach (TeleportPoint tp in bridgeTeleportPoints)
-                {
-                    tp.SetLocked(true);
-                }
-                foreach (TeleportPoint tp in storageRoomTeleportPoints)
-                {
-                    tp.SetLocked(true);
-                }
-                break;
-            case Room.storage:
-                if (humanManager.isTPavalaible)
-                {
-                    foreach (TeleportPoint tp in storageRoomTeleportPoints)
-                    {
-                        tp.SetLocked(false);
-                    }
-                }
-                else
-                {
-                    foreach (TeleportPoint tp in storageRoomTeleportPoints)
-                    {
-                        tp.SetLocked(true);
-                    }
-                }
+            foreach (TeleportPoint tp in storageRoomTeleportPoints)
+            {
+                tp.SetLocked(humanManager.roomID == Room.storage);
+                SetTPPoint(tp, humanManager.roomID == Room.storage);
+            }
+        } else
+        {
+            foreach (TeleportPoint tp in bridgeTeleportPoints)
+            {
+                tp.SetLocked(false);
+                SetTPPoint(tp, false);
+            }
 
-                foreach (TeleportPoint tp in controlRoomTeleportPoints)
-                {
-                    tp.SetLocked(true);
-                }
-                foreach (TeleportPoint tp in bridgeTeleportPoints)
-                {
-                    tp.SetLocked(true);
-                }
-                break;
+            foreach (TeleportPoint tp in controlRoomTeleportPoints)
+            {
+                SetTPPoint(tp, false);
+                    tp.SetLocked(false);
+            }
+
+            foreach (TeleportPoint tp in storageRoomTeleportPoints)
+            {
+                SetTPPoint(tp, false);
+                tp.SetLocked(false);
+            }
         }
+    }
+
+    private void SetTPPoint(TeleportMarkerBase tp, bool val)
+    {
+        if (tp.GetComponent<TeleportPoint>()) tp.GetComponent<TeleportPoint>().locked = val;
+        if (tp.GetComponent<TeleportArea>()) tp.GetComponent<TeleportArea>().locked = val;
     }
 }
